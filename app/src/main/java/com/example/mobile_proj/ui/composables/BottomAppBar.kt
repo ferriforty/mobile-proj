11ac4e9e1cdb43c4cec7cfac7982a16e806d693d
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2024-05-18 18:57:19 +0200

    Adjust FAB in HomeView

--- app/src/main/java/com/example/mobile_proj/ui/composables/BottomAppBar.kt
+++ app/src/main/java/com/example/mobile_proj/ui/composables/BottomAppBar.kt
@@ -1,19 +1,27 @@
 package com.example.mobile_proj.ui.composables
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.mobile_proj.ui.Route
 
 @Composable
 fun BottomAppBar(navController: NavHostController) {
-    BottomAppBar(actions = {
-        IconButton(onClick = { navController.navigate(Route.Profile.route)}) {
-            Icon(Icons.Outlined.AccountCircle, "Profile Screen")
+    BottomAppBar(
+        actions = {
+            IconButton(onClick = { navController.navigate(Route.Profile.route) }) {
+                Icon(Icons.Outlined.AccountCircle,
+                    "Profile Screen",
+                    modifier = Modifier.size(48.dp)
+                )
+            }
         }
-    })
+    )
 }
--- app/src/main/java/com/example/mobile_proj/ui/screens/home/HomeView.kt
+++ app/src/main/java/com/example/mobile_proj/ui/screens/home/HomeView.kt
@@ -1,13 +1,17 @@
 package com.example.mobile_proj.ui.screens.home
 
+import android.view.MotionEvent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -21,21 +25,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.mobile_proj.ui.Route
+import com.example.mobile_proj.ui.composables.BottomAppBar
+import com.example.mobile_proj.ui.composables.TopAppBar
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
     Scaffold(
+        topBar = { TopAppBar(navController, Route.Home) },
         floatingActionButton = {
-            FloatingActionButton(
-                containerColor = MaterialTheme.colorScheme.primary,
-                onClick = { navController.navigate(Route.Home.route) }
-            ) {
-                Icon(Icons.Outlined.Add, "Add Workout")
+            Box {
+                FloatingActionButton(
+                    onClick = { /* stub */ },
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(60.dp)
+                        .offset(y = 50.dp)
+                ) {
+                    Icon(Icons.Outlined.Add, "Add Workout")
+                }
             }
         },
-        floatingActionButtonPosition = FabPosition.Center
+        floatingActionButtonPosition = FabPosition.Center,
+        bottomBar = {
+                BottomAppBar(navController)
+        }
     ) { contentPadding ->
-        NoItemsPlaceholder(Modifier.padding(contentPadding))
+           NoItemsPlaceholder(modifier = Modifier
+               .padding(contentPadding)
+           )
     }
 }
 
@@ -44,7 +61,9 @@ fun NoItemsPlaceholder(modifier: Modifier = Modifier) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
         Icon(
             Icons.Outlined.Warning, "Warning",
@@ -63,5 +82,20 @@ fun NoItemsPlaceholder(modifier: Modifier = Modifier) {
             "Tap the + button to add a new workout.",
             style = MaterialTheme.typography.bodyLarge
         )
+        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+        Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
     }
+}
+
+fun isScrollingDown(event: MotionEvent): Boolean {
+    var mTouchPosition = 0.0F
+    var mReleasePosition = 0.0F
+    if (event.action == MotionEvent.ACTION_DOWN) {
+        mTouchPosition = event.y
+    }
+    if (event.action == MotionEvent.ACTION_UP) {
+        mReleasePosition = event.y
+    }
+    return mTouchPosition - mReleasePosition > 0
 }
