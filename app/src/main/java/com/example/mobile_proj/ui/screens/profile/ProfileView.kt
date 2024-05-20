6b677277897a7ca38ec6fdcbc6a6fd60312fd7a0
Giovanni Paradisi <giovanni.paradisi2@studio.unibo.it>
2024-05-20 18:15:55 +0200

    start profile view

--- app/src/main/java/com/example/mobile_proj/ui/screens/profile/ProfileView.kt
+++ app/src/main/java/com/example/mobile_proj/ui/screens/profile/ProfileView.kt
@@ -1,28 +1,49 @@
 package com.example.mobile_proj.ui.screens.profile
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Warning
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.mobile_proj.ui.Route
 import com.example.mobile_proj.ui.composables.TopAppBar
 
+
 @Composable
 fun ProfileScreen(navController: NavHostController) {
     Scaffold (
-        topBar = { TopAppBar(navController, Route.Profile) }
-    ){ contentPadding ->
-        Icon(Icons.Outlined.AccountCircle,
-            "Profile Image",
-            Modifier
-                .padding(contentPadding)
-                .size(45.dp)
-        )
-    }
+        topBar = { TopAppBar(navController, Route.Profile) },
+        content = { contentPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(contentPadding),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Row {
+                    Icon(Icons.Outlined.AccountCircle,
+                        "profile-image",
+                        modifier = Modifier.size(156.dp))
+                    Text("Name: MarioIlGrosso",
+                        modifier = Modifier.padding(top = 20.dp))
+                }
+                Divider(modifier = Modifier.padding(5.dp))
+            }
+        }
+    )
 }
