dfea6a61e73c1ed0872d63f2f7ab0356222164c7
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2024-05-27 14:12:47 +0200

    add try catch in main activity

--- app/src/main/java/com/example/mobile_proj/MainActivity.kt
+++ app/src/main/java/com/example/mobile_proj/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.example.mobile_proj
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BugReport
+import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.rememberNavController
 import com.example.mobile_proj.data.models.Theme
+import com.example.mobile_proj.database.AlertDialogConnection
 import com.example.mobile_proj.database.Connection
 import com.example.mobile_proj.ui.NavGraph
 import com.example.mobile_proj.ui.screens.settings.ThemeViewModel
 import com.example.mobile_proj.ui.theme.MobileprojTheme
+import io.realm.kotlin.mongodb.exceptions.InvalidCredentialsException
+import io.realm.kotlin.mongodb.exceptions.ServiceException
 import org.koin.androidx.compose.koinViewModel
 
 
@@ -30,6 +40,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             val themeViewModel = koinViewModel<ThemeViewModel>()
             val themeState by themeViewModel.state.collectAsStateWithLifecycle()
+            val openAlertDialog = remember { mutableStateOf(false) }
+            val openAlertDialogCreds = remember { mutableStateOf(false) }
 
             MobileprojTheme(darkTheme = when (themeState.theme) {
                 Theme.Light -> false
@@ -41,6 +53,39 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
+                    try {
+                        db.start()
+                    } catch (e: ServiceException) {
+                        openAlertDialog.value = true
+                    } catch (e: InvalidCredentialsException) {
+                        openAlertDialogCreds.value = true
+                    }
+
+                    when {
+                        openAlertDialog.value -> {
+                            AlertDialogConnection(
+                                onDismissRequest = { openAlertDialog.value = false },
+                                onConfirmation = { openAlertDialog.value = false },
+                                onDismissButton = { this.startActivity(Intent(Settings.ACTION_WIFI_SETTINGS))},
+                                dialogTitle = "Error in connecting to database",
+                                dialogText = "Check your wi-fi connection",
+                                icon = Icons.Default.Error
+                            )
+                        }
+                        openAlertDialogCreds.value -> {
+                            AlertDialogConnection(
+                                onDismissRequest = {},
+                                onConfirmation = {},
+                                onDismissButton = {},
+                                dialogTitle = "Wrong Credentials used to log in to server",
+                                dialogText = "Report the bug and a patch will soon arrive",
+                                icon = Icons.Default.BugReport
+                            )
+                        }
+                    }
+
+
                     val navController = rememberNavController()
                     Scaffold { contentPadding ->
                         NavGraph(
