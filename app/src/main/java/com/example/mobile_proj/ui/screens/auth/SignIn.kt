bd1ac38292efb6fa59ca90d724f7727703f7e4bf
Ettore Farinelli <ettore.farinelli@studio.unibo.it>
2024-05-30 15:34:57 +0200

    add logo and made cute the sign in and sign up page

--- app/src/main/java/com/example/mobile_proj/ui/screens/auth/SignIn.kt
+++ app/src/main/java/com/example/mobile_proj/ui/screens/auth/SignIn.kt
@@ -6,13 +6,16 @@ import android.content.Intent
 import android.provider.Settings
 import android.view.textclassifier.TextLinks.TextLink
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BugReport
@@ -29,10 +32,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.mobile_proj.MainActivity
+import com.example.mobile_proj.R
 import com.example.mobile_proj.database.AlertDialogConnection
 import com.example.mobile_proj.database.Connection
 import com.example.mobile_proj.ui.RouteAuth
@@ -79,6 +89,8 @@ fun SignIn(navController: NavHostController, db: Connection, intent: Intent) {
                 .fillMaxSize()
                 .padding(horizontal = 30.dp)
         ) {
+            Text(text = "Gym Shred", fontFamily = FontFamily.Cursive, fontSize = 70.sp, fontWeight = FontWeight.ExtraBold)
+            Spacer(modifier = Modifier.height(30.dp))
             LoginField(
                 value = credentials.username,
                 onChange = { data -> credentials = credentials.copy(username = data) },
@@ -134,6 +146,15 @@ fun SignIn(navController: NavHostController, db: Connection, intent: Intent) {
             ) {
                 Text("New user? Sign Up")
             }
+            Image(
+                painter = painterResource(id = R.drawable.ic_logo),
+                contentDescription = "app logo",
+                colorFilter = ColorFilter.tint(Color.Red),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(148.dp)
+                    .offset(x = 80.dp, y = 40.dp)
+            )
         }
     }
 }
